c5730d051a123e66a91ff848ea205de844212b40
q774833450 <774833450@qq.com>
2018-06-24 12:55:35 +0000

    User profile page

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,10 @@
 
 Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+/*
+ *
+ * Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+ * Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+ * Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+ */
