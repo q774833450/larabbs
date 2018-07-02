ff33d28ff12d3ade8ae5640481cde499a211241e
q774833450 <774833450@qq.com>
2018-07-02 01:13:17 +0000

    List user replies

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,4 +35,8 @@ class User extends Authenticatable
     {
         $this->notify(new ResetPassword($token));
     }
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
